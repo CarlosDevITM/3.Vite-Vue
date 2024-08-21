e989382b875d5e57fac0759bc0783b054702805a
CarlosDevITM <carlos.ponce@devitm.com>
2024-08-21 12:16:41 -0600

    Diseños de mensajes separados

--- src/components/chat/chatMessages.vue
+++ src/components/chat/chatMessages.vue
@@ -2,17 +2,16 @@
   <div class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
-      <!-- Example Message -->
-      <div class="flex justify-end">
-        <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">Hey, how's your day going?</div>
-      </div>
-
-      <!-- Example Received Message -->
-      <div class="flex">
-        <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-          Not too bad, just a bit busy. How about you?
-        </div>
-      </div>
+      <chatBubble :its-mine="true" message="Hola Mundo"></chatBubble>
+      <chatBubble
+        :its-mine="false"
+        message="No"
+        image="https://yesno.wtf/assets/no/15-7446b1035f784986609f456e15d30a5b.gif"
+      ></chatBubble>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import chatBubble from './chatBubble.vue';
+</script>
