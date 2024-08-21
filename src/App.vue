cab0d17e6a588ee84cae70e4edbadb44b6362bfa
CarlosDevITM <carlos.ponce@devitm.com>
2024-08-20 18:40:17 -0600

    Tailwind instalado y configurado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>Mi primera app con un componente independiente</h1>
 
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
   <squareCounter :value="5"></squareCounter>
   <squareCounterScript :value="5"></squareCounterScript>
   <squareCounterScriptSeparate :value="3"></squareCounterScriptSeparate>
--- src/components/SquareCounter.vue
+++ src/components/SquareCounter.vue
@@ -5,8 +5,8 @@
     <h3>Square: {{ squareCounter }}</h3>
 
     <div>
-      <button v-on:click="increase">+1</button>
-      <button @click="decrease">-1</button>
+      <button class="btn" v-on:click="increase">+1</button>
+      <button class="btn" @click="decrease">-1</button>
     </div>
   </section>
 </template>
@@ -14,9 +14,9 @@
 <!-- CSS-->
 <!-- Scoped: Es utilizado para segmentar los estilos de cada componente. Si tenemos, dos componentes ambos con un H1 y los usamos simultáneamente y queremos aplicar un estilo a todos los H1, este afectará tanto al primer como al segundo componente.
     Si se usa, se crea un Id para cada etiqueta, lo cual evita que otros estilos les afecten.-->
-<style scoped>
-h1 {
-  color: red;
+<style>
+.btn scoped {
+  @apply p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2;
 }
 </style>
 
