f870476671255afdaa4779e890908099d6efbec3
CarlosDevITM <carlos.ponce@devitm.com>
2024-08-21 18:06:30 -0600

    Proyecto finalizado

--- src/components/chat/chatMessages.vue
+++ src/components/chat/chatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <!-- <chatBubble :its-mine="true" message="Hola Mundo"></chatBubble> -->
@@ -19,9 +19,21 @@
 <script lang="ts" setup>
 import type { chatMessageInterface } from '@/interfaces/chatMessageInterface';
 import chatBubble from './chatBubble.vue';
+import { ref, watch } from 'vue';
 
 interface Props {
   messages: chatMessageInterface[];
 }
-defineProps<Props>();
+const { messages } = defineProps<Props>();
+
+const chatRef = ref<HTMLElement | null>(null);
+
+watch(messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>
